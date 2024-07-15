5ccb3eeb5a098f2ff52a34ed4be419b2e192fc8b
quang9d7 <nguyenlequang19120121@gmail.com>
2024-07-15 20:48:11 +0700

    user

--- migrate/sql/V1_0_1__insert_data.sql
+++ migrate/sql/V1_0_1__insert_data.sql
@@ -11,4 +11,3 @@ INSERT INTO user_subscriptions (subscriber_id, user_id) VALUES (1, 4);
 INSERT INTO user_subscriptions (subscriber_id, user_id) VALUES (1, 5);
 INSERT INTO user_subscriptions (subscriber_id, user_id) VALUES (2, 1);
 INSERT INTO user_subscriptions (subscriber_id, user_id) VALUES (5, 1);
-INSERT INTO user_subscriptions (subscriber_id, user_id) VALUES (7, 1);
